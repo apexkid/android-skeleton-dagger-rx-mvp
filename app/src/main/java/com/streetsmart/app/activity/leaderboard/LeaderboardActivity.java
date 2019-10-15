293a9fb4b949e597e1b7e1a59208a16879d2a59a
apexkid <apexkid369@gmail.com>
2019-10-15 17:47:47 +0530

    Added leaderboard UI and recycler

--- app/src/main/java/com/streetsmart/app/activity/leaderboard/LeaderboardActivity.java
+++ app/src/main/java/com/streetsmart/app/activity/leaderboard/LeaderboardActivity.java
@@ -2,16 +2,38 @@ package com.streetsmart.app.activity.leaderboard;
 
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.streetsmart.app.R;
 import com.streetsmart.app.activity.BaseActivity;
+import com.streetsmart.app.utils.LeaderboardDataSimulator;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LeaderboardActivity extends BaseActivity {
+
+
+    @BindView(R.id.leaderboard_recycler)
+    RecyclerView recyclerView;
+
+    private LeaderboardAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
+        ButterKnife.bind(this);
+
         initBottomNavigation(this, 2);
+
+        adapter = new LeaderboardAdapter(LeaderboardDataSimulator.getData());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 }
